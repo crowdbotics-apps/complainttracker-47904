0d2c85fb1b72d8a08a616b4d10c9e84d56d2c504
Crowdbotics <team@crowdbotics.com>
2024-05-14 13:48:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "complainttracker-47904",
+  url: "https://complainttracker-47904.botics.co",
+  api: "https://complainttracker-47904.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
